4c66816f56b83cae747ca09efdc13971f7962e0b
kevinjosephpaultwa <kevinjosephpaul.twa@gmail.com>
2021-07-12 12:49:47 +0530

    Temp

--- components/FeatureComponent/MainComponent.js
+++ components/FeatureComponent/MainComponent.js
@@ -3,6 +3,7 @@ import { View, Text, Button } from "react-native";
 
 const FeatureScreen = ({ route, navigation }) => {
   console.log(route.params);
+  const selectedValue = route.params.props.selected.data;
   return (
     <View>
       <Text>{route.params.props.selected.data}</Text>
